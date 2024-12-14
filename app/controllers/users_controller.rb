c882e2ad65c44efc21755d75328888d552d05290
topi0247 <karubona.ra247@gmail.com>
2024-12-14 17:01:02 +0900

    feat: usersコントローラのアクション作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def destroy; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
 end

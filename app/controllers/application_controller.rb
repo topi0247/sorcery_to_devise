72fe6a0a103ca814ea05fb74d85c7b1800d83baf
topi0247 <karubona.ra247@gmail.com>
2024-12-14 17:33:55 +0900

    feat: 認証しないと見れないよう追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger
+  before_action :require_login
+
+  def not_authenticated
+    redirect_to login_path, danger: 'ログインしてください'
+  end
 end

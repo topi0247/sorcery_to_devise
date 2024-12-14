9b0c5926c38a8b241e4aeba3fbce8d327389bda0
topi0247 <karubona.ra247@gmail.com>
2024-12-14 17:01:22 +0900

    feat: user_sessionコントローラのアクション作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,13 @@
 class UserSessionsController < ApplicationController
+  def new; end
+
+  def create; end
+
+  def destroy; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end

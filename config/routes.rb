7731fb469bea1d2c9c0578b7c838a03dc43a0afa
topi0247 <karubona.ra247@gmail.com>
2024-12-14 17:33:32 +0900

    fix: ログインとログアウトのパス修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: %i(index new create edit destroy)
-  resources :user_sessions, only: %i(new create destroy)
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
